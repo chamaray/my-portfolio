264cc0b807a6bf11195b19157b494dd6c157e8fe
Chamara Yaparathne <kanishkayaparathne@gmail.com>
2025-11-25 10:34:53 +0530

    Add Profile Details

--- src/content_option.js
+++ src/content_option.js
@@ -1,60 +1,60 @@
-const logotext = "JOHN";
+const logotext = "CHAMARA";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Chamara Yaparathne",
+    description: "I’m Chamara Yaparathne DevOps Engineer _ Full stack devloper_ Cloud Engineer,currently working in Sri Lanka",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Chamara Yaparathne",
     animated: {
         first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        second: "I love devops",
+        third: "I work on kubernates/containerization",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "Welcome to my portfolio! I am Chamara Kanishka, a passionate full-stack developer and problem-solver with expertise in building scalable, efficient, and user-friendly applications.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    aboutme: "I'm a passionate and detail-driven technology professional with a strong focus on DevOps, cloud engineering, automation, and modern infrastructure design. I enjoy building scalable, reliable, and secure systems that empower organizations to innovate faster and operate with confidence.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Software Engineer",
+        where: "Colombo",
+        date: "2024",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Senior Associate",
+        where: "Colombo",
+        date: "2023",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Trainee Software Engineer",
+        where: "Colombo",
+        date: "2023",
     },
 ];
 
 const skills = [{
-        name: "Python",
+        name: "JavaScript",
         value: 90,
     },
     {
-        name: "Djano",
+        name: "Java",
         value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
     },
     {
         name: "React",
-        value: 60,
+        value: 80,
     },
     {
-        name: "Jquery",
-        value: 85,
+        name: "Kubernates",
+        value: 80,
+    },
+    {
+        name: "Terraform",
+        value: 80,
     },
 ];
 
